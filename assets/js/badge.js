0421126a18f8832da204dfc96eb5c68cbd03ab1f
FDHoho007 <mail@fdhoho007.de>
2023-06-03 12:52:14 +0200

    Add basic badge view

--- assets/js/badge.js
+++ assets/js/badge.js
@@ -27,5 +27,35 @@ const Badge = {
         let meta = JSON.parse(localStorage.getItem("meta") || "{}");
         meta[bagde_public_key] = badgeMeta;
         localStorage.setItem("meta", JSON.stringify(meta));
+    },
+    loadBadges: async () => {
+        let badges = await Badge.allBadges();
+        let main = document.getElementsByTagName("main")[0];
+        for(let e of [...main.children])
+            if(e.classList.contains("collection"))
+                e.remove();
+        for(let collection in badges) {
+            let collection_div = document.createElement("div");
+            collection_div.classList.add("collection");
+            let collection_title = document.createElement("div");
+            collection_title.classList.add("title");
+            collection_title.innerText = collection.replaceAll("_", " ");
+            collection_div.appendChild(collection_title);
+            let collection_content = document.createElement("div");
+            collection_content.classList.add("content");
+            for(let badge_public_key in badges[collection]) {
+                let badge_img = document.createElement("img");
+                badge_img.src = Badge.HOST + "/svg/" + encodeURIComponent(badge_public_key) + ".svg";
+                if(badges[collection][badge_public_key].collected) {
+                    badge_img.classList.add("collected");
+                    badge_img.onclick = () => {};
+                }
+                else
+                    badge_img.onclick = () => {};
+                collection_content.appendChild(badge_img);
+            }
+            collection_div.appendChild(collection_content);
+            main.appendChild(collection_div);
+        }
     }
 }

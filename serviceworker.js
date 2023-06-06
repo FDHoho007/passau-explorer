a404ce22d4fd2bff70b06a2ad86d0b40990d109e
fdhoho007 <mail@fdhoho007.de>
2023-06-06 12:03:28 +0200

    Fix serviceworker to network first cache second

--- serviceworker.js
+++ serviceworker.js
@@ -1,14 +1,7 @@
-self.addEventListener('fetch', function (event) {
+self.addEventListener("fetch", function (event) {
     event.respondWith(
-        caches.open('dynamic').then(async function (cache) {
-            const response = await cache.match(event.request);
-            return (
-                response ||
-                fetch(event.request).then(function (response_1) {
-                    cache.put(event.request, response_1.clone());
-                    return response_1;
-                })
-            );
-        }),
-    );
-});
+        fetch(event.request).catch(function() {
+            return caches.match(event.request)
+        })
+    )
+})
